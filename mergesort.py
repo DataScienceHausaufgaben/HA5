5dbd1c2b0851a746e57fcf4a705e71ad46db1fd0
till.duensing <till.duensing@stud.uni-goettingen.de>
2023-06-18 16:42:59 +0200

    fixed mergesort.py

--- mergesort.py
+++ mergesort.py
@@ -1,51 +1,63 @@
-def ASSIGNMENT(new_list, i, old_list, j):
-    new_list[i] = old_list[j]
-
-
-def mergeSort(list_to_sort_by_merge):
-    if (
-        len(list_to_sort_by_merge) > 1
-        and not len(list_to_sort_by_merge) < 1
-        and len(list_to_sort_by_merge) != 0
-    ):
-        mid = len(list_to_sort_by_merge) // 2
-        left = list_to_sort_by_merge[:mid]
-        right = list_to_sort_by_merge[mid:]
-
-        mergeSort(left)
-        mergeSort(right)
-
-        l = 0
-        r = 0
-        i = 0
-
-        while l < len(left) and r < len(right):
-            if left[l] <= right[r]:
-                ASSIGNMENT(new_list=list_to_sort_by_merge, i=i, old_list=left, j=l)
-                l += 1
-            else:
-                ASSIGNMENT(new_list=list_to_sort_by_merge, i=i, old_list=right, j=r)
-                r += 1
-            i += 1
-
-        while l < len(left):
-            list_to_sort_by_merge[i] = left[l]
-            l += 1
-            i += 1
-
-        while r < len(right):
-            list_to_sort_by_merge[i] = right[r]
-            r += 1
-            i += 1
-
-
+# Importe
 import matplotlib.pyplot as plt
 
+# Methode zum Sortieren einer Liste mit mergesort
+def mergeSort(list):
+    # Ermittlung der Listenlänge
+    length = len(list)
+
+    # Überprüfung ob die Liste sortierbar ist
+    if (length <= 1):
+        return list
+    
+    # Mittiges Aufteilen der Liste 
+    mid = length // 2
+    left = list[:mid]
+    right = list[mid:]
+
+    # Rekursiver Aufruf mit den Teillisten
+    return merge(mergeSort(left),mergeSort(right))
+    
+# Sortiert und merged die Teillisten
+def merge(left ,right):
+    # Definieren der neuen List und Laufvariablen
+    merged = []
+    l,r = 0,0
+
+    # Sortieren 
+    while l < len(left) and r < len(right):
+        if left[l] <= right[r]:
+            merged.append(left[l])
+            l += 1
+        else:
+            merged.append(right[r])
+            r += 1
+
+    # Hinzufügen der übrigen Zahlen der Teillisten zur merged-Liste
+    while l < len(left):
+        merged.append(left[l])
+        l += 1
+
+    while r < len(right):
+        merged.append(right[r])
+        r += 1
+
+    return merged
+
+
+
+# Deklarierung der zu sortierenden Liste
 my_list = [54, 26, 93, 17, 77, 31, 44, 55, 20]
+
+# Visualisierung der unsortierten Liste
 x = range(len(my_list))
 plt.plot(x, my_list)
 plt.show()
-mergeSort(my_list)
+
+# Sortieren
+my_list = mergeSort(my_list)
+
+# Visualiserung der sortierten Liste
 x = range(len(my_list))
 plt.plot(x, my_list)
 plt.show()
